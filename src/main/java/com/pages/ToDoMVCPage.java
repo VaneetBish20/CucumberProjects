0bcaf72ec983a0926de4399d2f29a90638758e3b
Vaneet <vaneet.bish1@gmail.com>
2021-04-26 13:54:37 +0800

    removed comment

--- src/main/java/com/pages/ToDoMVCPage.java
+++ src/main/java/com/pages/ToDoMVCPage.java
@@ -14,8 +14,6 @@ public class ToDoMVCPage {
 	private By ToDoCount = By.xpath("//footer[@class='footer']//span[@class='todo-count']");
 	private By ToDoView = By.xpath("//li[@class='todo']//div[@class='view']");
 	
-	//private By signInButton = By.id("SubmitLogin");
-	
 	// 2. Constructor of the page class:
 		public ToDoMVCPage(WebDriver driver) {
 			this.driver = driver;
